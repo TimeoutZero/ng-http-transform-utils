cf6b751fe3d05db083787f1e8b18186e903cabfe
Carlos Henrique <carloshpds@gmail.com>
2018-06-04 06:29:12 -0300

    updating basebuild-component

--- src/entry/ngHttpTransformUtils.entry.js
+++ src/entry/ngHttpTransformUtils.entry.js
@@ -6,6 +6,8 @@
 // Importing main module file
 require('../app/index.module.coffee');
 
+require('file-loader?name=[name].[ext]!../../package.json');
+
 // Importing all component's scripts
 var components = require.context('../app/components', true, /.(coffee|cjsx|js|jsx)$/);
 components.keys().forEach(components);
